87da1d3593af399f3048f4c922e6d042b46efe3e
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-11-17 17:05:42 +0100

    wip

--- utils/get_name/main.tf
+++ utils/get_name/main.tf
@@ -2,15 +2,11 @@ variable "key" {
   description = "Name key"
 }
 
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
 data "external" "names" {
   program = [
     "slswtinternals",
     "get_name",
     "${path.root}",
-    "--region=${data.aws_region.current.name}",
     "--key=${var.key}"
   ]
 }
