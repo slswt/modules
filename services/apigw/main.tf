c2e336e6fc518e2f64aac360098bf8b065efab61
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-12-02 11:23:38 +0100

    wef

--- services/apigw/main.tf
+++ services/apigw/main.tf
@@ -11,9 +11,9 @@ locals {
   deployment_uri = "${format(
     "%s/aws/%s/aws_api_gateway_rest_api/%s",
     replace(path.root, "/^.*\\.Live\\/(.*)$/", ".Live/$1"),
-    "${data.aws_caller_identity.current.account_id}",
+    data.aws_caller_identity.current.account_id,
     "aws_api_gateway_rest_api",
-    "${data.aws_region.current.name}",
+    data.aws_region.current.name,
     var.id
   )}"
   name = "${md5(local.deployment_uri)}"
