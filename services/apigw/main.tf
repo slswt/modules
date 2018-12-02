ee7f98f1de78b79c068c3d7577ec6f11f5033d90
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-12-02 11:22:11 +0100

    ewf

--- services/apigw/main.tf
+++ services/apigw/main.tf
@@ -2,8 +2,21 @@ variable "id" {
   default = "default_id"
 }
 
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+
 locals {
-  name = "${md5(format("%s/api_gateway/%s", replace(path.root, "/^.*\\.Live\\/(.*)$/", ".Live/$1"), var.id))}"
+  deployment_uri = "${format(
+    "%s/aws/%s/aws_api_gateway_rest_api/%s",
+    replace(path.root, "/^.*\\.Live\\/(.*)$/", ".Live/$1"),
+    "${data.aws_caller_identity.current.account_id}",
+    "aws_api_gateway_rest_api",
+    "${data.aws_region.current.name}",
+    var.id
+  )}"
+  name = "${md5(local.deployment_uri)}"
 }
 
 resource "aws_api_gateway_rest_api" "screed_microservices" {
@@ -21,4 +34,9 @@ output "root_resource_id" {
 output "execution_arn" {
   value = "${aws_api_gateway_rest_api.screed_microservices.execution_arn}"
 }
+
+output "deployment_uri" {
+  value = "${local.deployment_uri}"
+}
+
 
