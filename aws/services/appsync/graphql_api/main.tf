123e1ca90da7ceec6939f8183fc69fd8a542bfb3
Richard Samuelsson <richard.samuelsson@redeye.se>
2018-12-02 18:59:27 +0100

    wip

---
+++ aws/services/appsync/graphql_api/main.tf
@@ -0,0 +1,19 @@
+variable "id" {
+}
+
+locals {
+  deployment_uri = "${format("%s/aws_appsync_graphql_api/%s", replace(path.root, "/^.*\\.Live\\/(.*)$/", "$1"), var.id)}"
+  name = "${md5(local.deployment_uri)}"
+}
+
+resource "aws_appsync_graphql_api" "graphql_api" {
+  authentication_type = "AWS_IAM"
+  name = "${local.name}"
+}
+
+output "api_id" {
+  value = "${aws_appsync_graphql_api.graphql_api.id}"
+}
+output "api_arn" {
+  value = "${aws_appsync_graphql_api.graphql_api.arn}"
+}
